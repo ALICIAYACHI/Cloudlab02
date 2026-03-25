606dc5ba043200bfd625134c27ed4b54f8f900f1
Alicia Yachi <alicia.yachi@tecsup.edu.pe>
2026-03-25 11:26:09 -0500

    cambios

--- conexion.php
+++ conexion.php
@@ -4,9 +4,9 @@
 
     $host = "host=dpg-d71v8h0gjchc739qbh80-a.oregon-postgres.render.com";
     $port = "port=5432";
-    $dbname = "test_m769";
-    $user = "test_m769_user";
-    $password = "LPEdZMH9wAw54CbbEUVLjMQLJMgzJhT6";
+    $dbname = "dbname=test_m769";
+    $user = "user=test_m769_user";
+    $password = "password=LPEdZMH9wAw54CbbEUVLjMQLJMgzJhT6";
 
     $db = pg_connect("$host $port $dbname $user $password");
 
